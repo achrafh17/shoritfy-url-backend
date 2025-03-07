78e7c6e5fe73719456b73c1d797c5e654c58b66b
Achraf <achrafhafid565@gmail.com>
2025-03-07 05:41:41 +0100

    again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL || "https://shoritfy-url-backend-production.up.railway.app"; 
+const BASE_URL = process.env.BASE_URL || "https://shorturl.com"; 
 
 app.use(express.json());
 app.use(cors());
